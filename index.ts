8a2c06e9d1c18f2cc47e8d3eb13cd05dd0fb1cf4
HomamZituni <homam93@msn.com>
2025-11-23 22:48:40 -0500

    added index function to fetch data

--- index.ts
+++ index.ts
@@ -1,1 +1,33 @@
 import { fetchProductCatalog, fetchProductReviews, fetchSalesReport, NetworkError, DataError } from './apiSimulator';
+
+const loadDashboard = () => {
+    fetchProductCatalog()
+        .then(products => {
+            console.log("Products:", products);
+
+            
+            return Promise.all(
+                products.map(product =>
+                    fetchProductReviews(product.id)
+                        .then(reviews => {
+                            console.log(`Reviews for ${product.name}:`, reviews);
+                        })
+                        .catch(err => {
+                            console.error(err.message);
+                        })
+                )
+            );
+        })
+        .then(() => fetchSalesReport())
+        .then(report => {
+            console.log("Sales Report:", report);
+        })
+        .catch(err => {
+            console.error("Error in dashboard:", err.message);
+        })
+        .finally(() => {
+            console.log("All API calls attempted.");
+        });
+};
+
+loadDashboard();
